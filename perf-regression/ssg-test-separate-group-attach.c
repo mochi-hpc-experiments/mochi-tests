b0d10c5c548809b5cfbb62bf2616c440b65ad0e4
Rob Latham <robl@mcs.anl.gov>
2019-12-10 22:55:46 -0600

    Add "ssg from separate processes" test from ssg repo

---
+++ perf-regression/ssg-test-separate-group-attach.c
@@ -0,0 +1,31 @@
+#include <mpi.h>
+#include <ssg.h>
+#include <assert.h>
+
+int main(int argc, char **argv)
+{
+    int ret;
+    ssg_group_id_t gid;
+    margo_instance_id mid;
+
+    MPI_Init(&argc, &argv);
+
+    mid = margo_init(argv[1], MARGO_CLIENT_MODE, 0, 1);
+    ssg_init();
+
+    int count=1;
+    ret = ssg_group_id_load(argv[2], &count, &gid);
+    assert (ret == SSG_SUCCESS);
+
+    fprintf(stderr, "        attaching...\n");
+    ret = ssg_group_observe(mid, gid);
+    fprintf(stderr, "        attached...\n");
+
+    fprintf(stderr, "        dumping...\n");
+    ssg_group_dump(gid);
+    fprintf(stderr, "        dumped...\n");
+
+    ssg_group_unobserve(gid);
+    margo_finalize(mid);
+    MPI_Finalize();
+}
