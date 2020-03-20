d4f961335655defc7bd2727e0a9ee55713db3721
Rob Latham <robl@mcs.anl.gov>
2020-03-20 16:11:46 -0500

    new spack version

--- perf-regression/theta/run-regression.sh
+++ perf-regression/theta/run-regression.sh
@@ -37,7 +37,7 @@ cp ${ORIGIN}/*.qsub ${JOBDIR}
 # set up build environment
 cd $SANDBOX
 git clone -q https://github.com/spack/spack.git
-(cd spack && git checkout -b spack-0.14 v0.14.0)
+(cd spack && git checkout -b spack-0.14.1 v0.14.1)
 git clone -q https://xgitlab.cels.anl.gov/sds/sds-repo.git
 git clone -q https://xgitlab.cels.anl.gov/sds/sds-tests.git
 
