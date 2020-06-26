173aca25507b9241a8587949447d5ca308367186
Phil Carns <carns@mcs.anl.gov>
2020-06-26 13:00:12 -0400

    pthread to drive hg progress in non-margo path

--- interoperability/lib-nm.c
+++ interoperability/lib-nm.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <pthread.h>
 
 #include <mercury.h>
 
@@ -15,18 +16,59 @@
 
 #define NM_ID 1
 
+static int shutdown_flag = 0;
+
+struct progress_fn_args
+{
+    hg_context_t *context;
+};
+
+void* progress_fn(void* _arg)
+{
+    struct progress_fn_args *pargs = _arg;
+    int ret;
+    unsigned int actual_count;
+
+    while(!shutdown_flag)
+    {
+        do{
+            ret = HG_Trigger(pargs->context, 0, 1, &actual_count);
+        }while((ret == HG_SUCCESS) && actual_count);
+
+        if(!shutdown_flag)
+            ret = HG_Progress(pargs->context, 100);
+
+        if(ret != HG_SUCCESS && ret != HG_TIMEOUT)
+        {
+            fprintf(stderr, "Error: unexpected HG_Progress() error code %d\n", ret);
+            assert(0);
+        }
+    }
+
+    return(NULL);
+}
+
 void* nm_run_client(void* _arg)
 {
     struct nm_client_args *nm_args = _arg;
-    hg_context_t *context;
+    struct progress_fn_args pargs;
+    pthread_t tid;
+    int ret;
 
     /* create separate context for this component */
-    context = HG_Context_create_id(nm_args->class, NM_ID);
-    assert(context);
+    pargs.context = HG_Context_create_id(nm_args->class, NM_ID);
+    assert(pargs.context);
+
+    /* create thread to drive progress */
+    ret = pthread_create(&tid, NULL, progress_fn, &pargs);
+    assert(ret == 0);
 
     sleep(1);
 
-    HG_Context_destroy(context);
+    shutdown_flag = 1;
+    pthread_join(tid, NULL);
+
+    HG_Context_destroy(pargs.context);
 
     return(NULL);
 }
@@ -34,15 +76,24 @@ void* nm_run_client(void* _arg)
 void* nm_run_server(void* _arg)
 {
     struct nm_server_args *nm_args = _arg;
-    hg_context_t *context;
+    struct progress_fn_args pargs;
+    pthread_t tid;
+    int ret;
 
     /* create separate context for this component */
-    context = HG_Context_create_id(nm_args->class, NM_ID);
-    assert(context);
+    pargs.context = HG_Context_create_id(nm_args->class, NM_ID);
+    assert(pargs.context);
+
+    /* create thread to drive progress */
+    ret = pthread_create(&tid, NULL, progress_fn, &pargs);
+    assert(ret == 0);
 
     sleep(1);
 
-    HG_Context_destroy(context);
+    shutdown_flag = 1;
+    pthread_join(tid, NULL);
+
+    HG_Context_destroy(pargs.context);
 
     return(NULL);
 }
