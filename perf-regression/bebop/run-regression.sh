82a653614c3ee43d730041434a6a2fd0e4c24912
Rob Latham <robl@mcs.anl.gov>
2020-07-06 09:59:16 -0500

    pick up proper commit for intel mpi bugfix

--- perf-regression/bebop/run-regression.sh
+++ perf-regression/bebop/run-regression.sh
@@ -39,7 +39,7 @@ cd $SANDBOX
 git clone -q https://github.com/spack/spack.git
 (cd spack && git checkout -b spack-0.15.0 v0.15.0)
 # 0.15.0 bugfix: https://github.com/spack/spack/issues/17371 : Intel-MPI vs Intel-Mpi)
-(cd spack && git cherry-pick c6b56a268260923ec834392d365ba4cb3402dbc8)
+(cd spack && git cherry-pick e9e3e88f637dcaa8e3d7a463c6af03ce77c49ce1)
 git clone -q https://xgitlab.cels.anl.gov/sds/sds-repo.git
 git clone -q https://xgitlab.cels.anl.gov/sds/sds-tests.git
 
