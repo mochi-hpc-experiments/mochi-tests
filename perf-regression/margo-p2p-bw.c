bb9184a0496a484d193255f83f7777ed98a367c3
Rob Latham <robl@mcs.anl.gov>
2021-07-07 13:00:09 -0400

    Summit required stdlib

--- perf-regression/margo-p2p-bw.c
+++ perf-regression/margo-p2p-bw.c
@@ -18,6 +18,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
--- perf-regression/margo-p2p-vector.c
+++ perf-regression/margo-p2p-vector.c
@@ -30,6 +30,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <errno.h>
 
