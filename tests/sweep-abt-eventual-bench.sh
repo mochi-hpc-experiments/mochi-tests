e095b7cce541a5bd5df41191c0d9e843c9771e96
Rob Latham <robl@mcs.anl.gov>
2022-02-08 16:58:59 -0500

    tweak eventual sweep script for scale/platforms
    - On Summit and Crusher we don't run jobs directly we use 'jsrun' or srun', so
      take the whole job launch command as an argument
    - interesting results don't start happening until we get past 1000 ULTs: we
      don't want to run a ton more tests so also increase the step.

--- tests/sweep-abt-eventual-bench.sh
+++ tests/sweep-abt-eventual-bench.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -eq 0 ]; then
     echo "Usage: sweep-abt-eventual-bench.sh <executable>"
     exit 1
 fi
 
-EXE=$1
+EXE=$@
 OUTFILE=sweep-abt-eventual-bench.$$.dat
 
 echo writing results to $OUTFILE
 
-# sweep through number of ULTs in increments of 100 from 100 to 1000
-for i in `seq 100 100 1000`; do
+# sweep through number of ULTs in increments of 100 from 250 to 4850 (20 points)
+for  i in `seq 100 250 4850`; do
     echo ... running benchmark with $i ULTs
     $EXE -n $i >> $OUTFILE
 done
