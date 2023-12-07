c0ea16e1e733925d4ea197bb3aacd7d9347491d4
Phil Carns <carns@mcs.anl.gov>
2023-12-07 01:55:45 +0000

    add baseline margo and vector transfer tests

--- perf-regression/polaris/run-regression.sh
+++ perf-regression/polaris/run-regression.sh
@@ -57,11 +57,15 @@ echo "=== SUBMIT AND WAIT FOR JOBS ==="
 cd $PREFIX/bin
 export SANDBOX
 
-qsub -W block=true -v SANDBOX -o gpu.out -e gpu.err ./run_gpu_margo_p2p_bw.qsub
+# continue on these even if a job fails so that we get emails with partial
+# results
+qsub -W block=true -v SANDBOX -o gpu.out -e gpu.err ./run_gpu_margo_p2p_bw.qsub || true
+qsub -W block=true -v SANDBOX -o margo.out -e margo.err ./margo-regression.qsub || true
+qsub -W block=true -v SANDBOX -o vector.out -e vector.err ./margo-vector-regression.qsub || true
 
 echo "=== JOB DONE, COLLECTING AND SENDING RESULTS ==="
 # gather output, strip out funny characters, mail
-cat gpu.err gpu.out > combined.$JOBID.txt
+cat margo.err margo.out vector.err vector.out gpu.err gpu.out > combined.$JOBID.txt
 dos2unix combined.$JOBID.txt
 mailx -s "mochi-regression (polaris)" sds-commits@lists.mcs.anl.gov < combined.$JOBID.txt
 cat combined.$JOBID.txt
